07c38e00f93f6d7b41440e47ad9ab1af49636dfc
Baran Karakaya <thebarankarakaya@gmail.com>
2024-06-04 10:45:46 +0300

    Added 32 or 64 bit control to work without issues.

--- Installer.cs
+++ Installer.cs
@@ -7,19 +7,16 @@ namespace vcredist_installer
 {
     public class Installer
     {
-        static void PrintTitle()
-        {
-            Console.Title = "Visual C++ Redistributables All-in-One Package Installer - by barankrky";
-            Console.WriteLine("\n     Visual C++ Redistributable Installer by barankrky\n     https://github.com/barankrky/vcredist_installer \n");
+        static string console_title = "Visual C++ vcredists All-in-One Package Installer - by barankrky";
+        static string app_title = "\n     Visual C++ Redistributable Installer by barankrky\n     https://github.com/barankrky/vcredist_installer \n";
 
-        }
-        static void CheckRedistributables()
+        static void CheckVCRedists()
         {
-            Console.WriteLine("- Checking redistributables...");
+            Console.WriteLine("- Checking vcredists...");
             Thread.Sleep(250);
-            if (Directory.Exists("redistributables"))
+            if (Directory.Exists("vcredists"))
             {
-                DirectoryInfo package_dir = new DirectoryInfo("redistributables");
+                DirectoryInfo package_dir = new DirectoryInfo("vcredists");
                 FileInfo[] package_files = package_dir.GetFiles("*vcredist*.exe");
                 foreach (FileInfo file in package_files)
                 {
@@ -29,18 +26,18 @@ namespace vcredist_installer
             }
             else
             {
-                Console.WriteLine("-- redistributables not found. Please ensure that there is a 'redistributables' folder in the location where you run the application.");
+                Console.WriteLine("-- vcredists not found. Please ensure that there is a 'vcredists' folder in the location where you run the application.");
                 Console.ReadLine();
                 Environment.Exit(0);
             }
 
             Console.Clear();
-            PrintTitle();
+            Console.WriteLine(app_title);
 
         }
         static void Execute(string fileName, string args)
         {
-            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory + "redistributables";
+            Environment.CurrentDirectory = AppDomain.CurrentDomain.BaseDirectory + "vcredists";
             Console.WriteLine("-> Installing " + fileName + "...");
             ProcessStartInfo psi = new ProcessStartInfo();
             psi.FileName = "cmd.exe";
@@ -56,42 +53,72 @@ namespace vcredist_installer
             proc.Start();
             proc.WaitForExit();
         }
-
-        static void Installx32()
+        static void Install()
         {
-            // Will be checked later
-        }
+            bool is64bit = Environment.Is64BitOperatingSystem;
+            string vcredist_version = "all";
 
-        static void Installx64(string vcredist_version)
-        {
-            switch (vcredist_version)
+            switch (is64bit)
             {
-                case "all":
-                    Execute("vcredist2005_x86.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
-                    Execute("vcredist2005_x64.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
+                case false:
+                    switch (vcredist_version)
+                    {
+                        case "all":
+                            Execute("vcredist2005_x86.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
 
-                    Execute("vcredist2008_x86.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
-                    Execute("vcredist2008_x64.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
+                            Execute("vcredist2008_x86.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
 
-                    Execute("vcredist2010_x86.exe", @"/q");
-                    Execute("vcredist2010_x64.exe", @"/q");
+                            Execute("vcredist2010_x86.exe", @"/q");
 
-                    Execute("vcredist2012_x86.exe", @"/install /quiet /norestart");
-                    Execute("vcredist2012_x64.exe", @"/install /quiet /norestart");
+                            Execute("vcredist2012_x86.exe", @"/install /quiet /norestart");
 
-                    Execute("vcredist2013_x86.exe", @"/install /quiet /norestart");
-                    Execute("vcredist2013_x64.exe", @"/install /quiet /norestart");
+                            Execute("vcredist2013_x86.exe", @"/install /quiet /norestart");
 
-                    Execute("vcredist2015_2017_2019_2022_x86.exe", @"/install /quiet /norestart");
-                    Execute("vcredist2015_2017_2019_2022_x64.exe", @"/install /quiet /norestart");
+                            Execute("vcredist2015_2017_2019_2022_x86.exe", @"/install /quiet /norestart");
+                            break;
+
+                        default:
+                            break;
+                    }
+                    break;
+
+                case true:
+                    switch (vcredist_version)
+                    {
+                        case "all":
+                            Execute("vcredist2005_x86.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
+                            Execute("vcredist2005_x64.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
+
+                            Execute("vcredist2008_x86.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
+                            Execute("vcredist2008_x64.exe", @"/q:a /c:""msiexec /i vcredist.msi /qn""");
+
+                            Execute("vcredist2010_x86.exe", @"/q");
+                            Execute("vcredist2010_x64.exe", @"/q");
+
+                            Execute("vcredist2012_x86.exe", @"/install /quiet /norestart");
+                            Execute("vcredist2012_x64.exe", @"/install /quiet /norestart");
+
+                            Execute("vcredist2013_x86.exe", @"/install /quiet /norestart");
+                            Execute("vcredist2013_x64.exe", @"/install /quiet /norestart");
+
+                            Execute("vcredist2015_2017_2019_2022_x86.exe", @"/install /quiet /norestart");
+                            Execute("vcredist2015_2017_2019_2022_x64.exe", @"/install /quiet /norestart");
+                            break;
+
+                        default:
+                            break;
+                    }
                     break;
 
                 default:
                     break;
             }
 
+
+            
+
             Console.Clear();
-            PrintTitle();
+            Console.WriteLine(app_title);
             Console.WriteLine(">> All vcredists are installed completely.");
             Console.WriteLine(">> Closing in 3 seconds.");
             Thread.Sleep(2800);
@@ -101,16 +128,16 @@ namespace vcredist_installer
 
         static void Initialize()
         {
-            PrintTitle();
-            // Check vcredist redistributables
-            CheckRedistributables();
+            Console.Title = console_title;
+            // Check vcredist vcredists
+            CheckVCRedists();
 
         }
 
         public static void Start()
         {
             Initialize();
-            Installx64("all");
+            Install();
         }
     }
 }
