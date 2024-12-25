829eee5635870fcc12bc85f2705fd031cd90b78e
Baran Karakaya <thebarankarakaya@gmail.com>
2024-12-25 23:54:22 +0300

    Update main.py : Updated title

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import os, sys
+import os
+import redist_manager as rm
 
-def start():
-    pass
+title = """--------------------------------------------------------------------------------
+  Microsoft Visual C++ Runtimes All-in-One Installer by barankrky 
+  https://github.com/barankrky/vcredist_installer 
+--------------------------------------------------------------------------------
+"""
 
 if __name__ == '__main__':
-    start()
+    os.system("title Microsoft Visual C++ Runtimes All-in-One Installer by barankrky : Last Update = November 2024")
+    os.system('cls')
+    print(title)
+    rm.start()
