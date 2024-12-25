9325833928c2a72be6051b2d8b56ea10e3a0e8da
Baran Karakaya <thebarankarakaya@gmail.com>
2024-12-26 01:16:05 +0300

    Add vcredist.ps1

---
+++ vcredist.ps1
@@ -0,0 +1,8 @@
+﻿$downloadUrl = "https://github.com/barankrky/vcredist_installer/raw/refs/heads/master/vcredist_installer.exe"
+$destinationPath = "$env:TEMP\vcredist_installer.exe"
+Invoke-WebRequest -Uri $downloadUrl -OutFile $destinationPath
+
+if (Test-Path $destinationPath) {
+    Start-Process -NoNewWindow -FilePath $destinationPath
+} else {
+}
