0f4888cb491497e47b27301195e07c290b99ad1b
Baran Karakaya <thebarankarakaya@gmail.com>
2024-12-27 00:50:14 +0300

    Update vcredist.ps1 : Update download and call function

--- vcredist.ps1
+++ vcredist.ps1
@@ -1,7 +1,4 @@
-﻿$url = "https://github.com/barankrky/vcredist_installer/raw/refs/heads/master/vcredist_installer.exe"
-$path = "$env:TEMP\vcredist_installer.exe"
+﻿
+Invoke-WebRequest -Uri "https://github.com/barankrky/vcredist_installer/raw/refs/heads/master/vcredist_installer.exe" -OutFile "$env:TEMP\vcredist_installer.exe"
+& "$env:TEMP\vcredist_installer.exe"
 
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($url, $path)
-
-& $destinationPath
